70bfd8725b22e31655c357bdf52421acbe508d77
Ung Nguyen <ung.nguyen@saritasa.com>
2023-06-18 13:51:58 +0700

    Unit test auth middleware

--- test/authMiddleware.js
+++ test/authMiddleware.js
@@ -7,6 +7,10 @@ const { loadEnvironmentVariables } = require('./../config/env')
 loadEnvironmentVariables()
 
 describe('Auth middleware', function () {
+    afterEach(() => {
+        sinon.restore(); // Restore stubs and spies after each test case
+    })
+
     it('should throw error if no authorization header is provided', function () {
         const req = {
             get: sinon.stub().returns(null),
@@ -26,7 +30,7 @@ describe('Auth middleware', function () {
             get: sinon.stub().returns('NotBearer sampleToken'),
         }
         expect(authMiddleware.bind(this, req, {}, () => { })).to.throw(Error, 'Invalid token type.')
-        .with.property('statusCode', 403)
+            .with.property('statusCode', 403)
     })
 
     it('should throw 500 error if jwt exception', function () {
@@ -37,7 +41,19 @@ describe('Auth middleware', function () {
         verifyStub.throws({
             statusCode: 500
         })
-        verifyStub.restore()
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw()
+            .with.property('statusCode', 500)
+    })
+
+    it('should throw 401 error if decoded jwt null', function () {
+        const req = {
+            get: sinon.stub().returns('Bearer sampleToken'),
+        }
+        const verifyStub = sinon.stub(jwt, 'verify')
+        verifyStub.returns(null)
+
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw(Error, 'Not authenticated.')
+            .with.property('statusCode', 401)
     })
 
     it('should throw error if the token is invalid', function () {
@@ -57,6 +73,45 @@ describe('Auth middleware', function () {
         expect(authMiddleware.bind(this, req, {}, () => { })).to.throw()
     })
 
+    it('should verify successfully if token is valid', () => {
+        const token = jwt.sign(
+            {
+                email: 'test@example.com',
+                userId: 'userId'
+            },
+            process.env.APP_SECRET_KEY,
+            { expiresIn: '1h' }
+        )
+        const req = {
+            get: function (headerName) {
+                return 'Bearer ' + token
+            }
+        }
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.not.throw()
+    })
+
+    it('should throw error if token is expired', () => {
+        const clock = sinon.useFakeTimers()
+        const expiresIn = 1; // Set expiration time to 1 second
+        const secretKey = process.env.APP_SECRET_KEY
+        const token = jwt.sign(
+            {
+                email: 'test@example.com',
+                userId: 'userId'
+            },
+            secretKey,
+            { expiresIn }
+        )
+        clock.tick(2000)
+        const req = {
+            get: function (headerName) {
+                return 'Bearer ' + token
+            }
+        }
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw()
+        clock.restore()
+    })
+
     it('token after decode should contain userId', function () {
         const req = {
             get: function (headerName) {
@@ -67,10 +122,12 @@ describe('Auth middleware', function () {
         jwt.verify.returns({
             userId: 'ok',
         })
-        authMiddleware(req, {}, () => {})
+
+        const nextSpy = sinon.spy()
+        authMiddleware(req, {}, nextSpy)
         expect(req).to.have.property('userId')
         expect(req).to.have.property('userId', 'ok')
         expect(jwt.verify.called).to.be.true
-        jwt.verify.restore()
+        expect(nextSpy.calledOnce)
     })
 })
