f03a60969e1d988f705ca416d12127cb4399c120
Ung Nguyen <ung.nguyen@saritasa.com>
2023-06-20 17:54:25 +0700

    Test controller

---
+++ test/authController.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect
+const sinon = require('sinon')
+const assert = require('assert')
+
+const User = require('./../models/user')
+const AuthController = require('./../controllers/auth')
+
+describe('AuthController - Login', () => {
+    afterEach(() => {
+        sinon.restore()
+    })
+
+    it('should throw 500 error when access database fails', async () => {
+        const req = {
+            body: {
+                email: 'foo@bar.com',
+                password: 'password'
+            }
+        }
+        sinon.stub(User, 'findOne')
+        User.findOne.throws()
+
+        try {
+            const result = await AuthController.login(req, {}, () => { })
+            expect(result).to.be.an('error')
+            expect(result).to.have.property('statusCode', 500)
+        } catch (error) {
+            assert.fail(error)
+        }
+    })
+})
